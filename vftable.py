4452462ec640900c3c8afd6c3cde3d55acd8e913
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-02 02:49:03 +1300

    add vftable struct generator

---
+++ vftable.py
@@ -0,0 +1,90 @@
+# TODO write a description for this script
+# @author NathanSnail
+# @category _Custom
+# @keybinding
+# @menupath
+# @toolbar
+
+import ghidra
+from ghidra.app.decompiler import DecompInterface
+from ghidra.app.decompiler.flatapi import FlatDecompilerAPI
+from ghidra.app.script import GhidraState
+from ghidra.app.util.cparser.C import CParser
+from ghidra.program.flatapi import FlatProgramAPI
+from ghidra.program.model.address import Address
+from ghidra.program.model.data import (ArrayDataType, CategoryPath, DataType,
+                                       DataTypeConflictHandler,
+                                       DataTypeManager,
+                                       FunctionDefinitionDataType,
+                                       PointerDataType, PointerType,
+                                       StringDataType, StructureDataType)
+from ghidra.program.model.listing import Data, FunctionSignature, Program
+from ghidra.program.model.symbol import SymbolType
+from ghidra.util.task import TaskMonitor
+
+
+def log(x):
+    print(type(x), x)
+
+
+def get_state():
+    # type: () -> GhidraState
+    return getState()
+
+
+def get_addr():
+    # type: () -> Address
+    return currentAddress
+
+
+def hex_n(n):
+    if n[0:2] == "0x":
+        num = int(n, 16)
+    else:
+        num = int(n)
+    return num
+
+
+state = get_state()
+addr = get_addr()
+program = state.getCurrentProgram()
+listing = program.getListing()
+dman = program.getDataTypeManager()
+fpapi = FlatProgramAPI(program)
+fdapi = FlatDecompilerAPI(fpapi)
+fman = program.getFunctionManager()
+symt = program.getSymbolTable()
+decomp_i = DecompInterface()
+decomp_i.openProgram(program)
+
+sym = symt.getPrimarySymbol(addr)
+path = ""
+namespace = sym.getParentNamespace()
+while namespace and namespace.getParentNamespace() and not namespace.isGlobal():
+    path += "/" + namespace.getName()
+    namespace = namespace.getParentNamespace()
+
+data = listing.getDataAt(addr)
+data_type = data.getDataType()
+print(data_type, data_type.length)
+fields = []
+cat = CategoryPath(path + "/vftable")
+for i in range(data_type.length // 4):
+    item = data.getComponentAt(i * 4)
+    function_address = item.getValue()
+    function = fpapi.getFunctionAt(function_address)
+    prototype = decomp_i.decompileFunction(function, 30, None).getHighFunction().getFunctionPrototype()
+    print(prototype)
+    fn_t = FunctionDefinitionDataType(function.getSignature())
+    fn_t.setReturnType(prototype.returnType)
+    fn_t.setCategoryPath(cat)
+    dt = dman.addDataType(fn_t, DataTypeConflictHandler.REPLACE_HANDLER)
+    fields.append((dt, function.getName()))
+
+print(fields)
+struct = StructureDataType("vftable_struct", len(fields) * 4)
+struct.setCategoryPath(cat)
+for k, f in enumerate(fields):
+    print(f)
+    struct.replaceAtOffset(k * 4, PointerDataType(f[0]), 4, f[1], "")
+dt = dman.addDataType(struct, DataTypeConflictHandler.REPLACE_HANDLER)
